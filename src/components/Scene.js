32aec8cd0bd5790624c48a2cb9c0022da4a9985e
Cristian Alegre Bustos <cnalegrebustos@teco.com.ar>
2022-09-30 09:43:15 -0300

    Animacion open door ready

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,33 +1,10 @@
 import { useFrame,Canvas,useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import {useRef,useState} from 'react'
-import { XR, ARButton} from '@react-three/xr'
+import { XR, ARButton, Interactive} from '@react-three/xr'
 import Tarjeta from '../models/Tarjeta'
 
-//import Scene from './Scene'
 
-function Box(props) {
-  // This reference will give us direct access to the mesh
-  const mesh = useRef()
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (mesh.current.rotation.x += 0.01))
-  // Return view, these are regular three.js elements expressed in JSX
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
-  )
-}
 
 const Scene=({Onclick})=>{
     return(
@@ -38,9 +15,10 @@ const Scene=({Onclick})=>{
         referenceSpace="local">
         <ambientLight />
          <pointLight position={[10, 10, 10]} />
-          {/* <Box position={[-1.2, 0, 0]} />
-          <Box position={[1.2, 0, 0]} />   */}
-          <Tarjeta onSelect={console.log("HOLA")} position={[0,0,-1]} />
+          <Interactive onSelect={()=>console.log("HOLA")}>
+          <Tarjeta  position={[0,0,-1.5]}  />
+          </Interactive>
+         
           </XR>
       </Canvas>
       </>
--- src/models/Tarjeta.js
+++ src/models/Tarjeta.js
@@ -1,26 +1,24 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
+import * as THREE from 'three'
 
 export default function Tarjeta(props) {
   const { nodes, materials } = useGLTF(
     "https://cristianalegrebustos.github.io/WebAr/webxlWorkshop/assets/tarjeta3d.glb"
   );
-  let i=0;
-// This reference will give us direct access to the card
-const tarjeta = useRef();
-// Subscribe this component to the render-loop, rotate the mesh every frame
-useFrame((state, delta) => {
-  
-    console.log(tarjeta.current.rotation.z);
-    console.log(i)
 
-    
-   tarjeta.current.rotation.y = Math.sin(Date.now() * 0.001) * Math.PI * 0.2;
+  const tarjeta= useRef()
 
-})
+  useFrame((state)=>{
+    const t = state.clock.getElapsedTime()
+    tarjeta.current.rotation.y =  (1 + Math.sin(t)) / 3
+}
+
+  )
+
   return (
-    <group  onSelect={console.log("HOLA3")} {...props} dispose={null} ref={tarjeta} >
+    <group  {...props} dispose={null} ref={tarjeta} >
       {/* <group position={[0, 0, 0.28]} scale={[0.33, 0.51, 0]}>
         <mesh
           castShadow
@@ -29,9 +27,9 @@ useFrame((state, delta) => {
           material={materials["33"]}
         />
       </group> */}
-      <group  onSelect={console.log("HOLA2")} position={[0, 0, 0.25]} scale={[0.31, 0.49, 0.24]}>
+      <group position={[0, 0, 0.25]} scale={[0.31, 0.49, 0.24]}>
         <mesh
-          onSelect={console.log("HOLA")}
+          onClick={()=>console.log("HOLA")}
           castShadow
           receiveShadow
           geometry={nodes.node_id8.geometry}
